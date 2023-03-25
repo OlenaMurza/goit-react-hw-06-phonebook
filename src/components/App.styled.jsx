70b98fb97d5fe807c75b390e0350429323bb3c4b
OlenaMurza <murzaelena@gmail.com>
2023-03-25 19:16:55 +0200

    add

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+
+export const Container = styled.div`
+  padding: 15px 30px;
+`;
+
+export const Section = styled.section`
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
+
+export const TitlePhone = styled.h1`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const TitleContacts = styled.h2`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: bold;
+`;
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+export const Form = styled.form`
+  max-width: 360px;
+  border-radius: 5px;
+  padding: 10px;
+  border: 2px solid grey;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+  margin-top: 2px;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+`;
